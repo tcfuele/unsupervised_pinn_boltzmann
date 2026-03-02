5eaae8f3e1afe56803dd024959670c7dc7065854
tcfuele <timo.fuele@gmail.com>
2026-03-02 14:28:54 +0100

    First fully functional version. Needs tweaking

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # unsupervised_pinn_boltzmann
 
-Can a physics-informed neural network approximate the solution of the 1D Boltzmann–BGK equation without supervised data while preserving macroscopic conservation laws?
+This project was created to answer the question: "Can a physics-informed neural network approximate the solution of the 1D Boltzmann–BGK equation without supervised data while preserving macroscopic conservation laws?"
+It is indeed possible to solve this and there is very interesting insight to be gained from that. Currently I am working on a physics notebook to provide further information on that, as well as tweaking the parameters. Otherwise the model is fully functional and you can run it on your own machine (see Usage).
+
+The model will train an unsupervised PINN with loss terms enforcing right behaviour of: PDE, boundary condition, initial condition and moment conservation.
+The initial condition looks like a flat density distribution with a small cosine perturbation:
+
+![](https://github.com/tcfuele/unsupervised_pinn_boltzmann/figures/moments_12000.jpg)
+
+
+One can see an offset here since the distribution function is translational invariant.
+The learned distribution at initial time can be visualized in the phase space like this:
+
+![](https://github.com/tcfuele/unsupervised_pinn_boltzmann/figures/f_12000.jpg)
+
+
+Also I provide a small gif generator to look at the time evolution of the density function of the system:
+
+![](https://github.com/tcfuele/unsupervised_pinn_boltzmann/figures/ani_12000.gif)
+
+
+## Usage
+
+The model is run by executing `training.py` as main:
+
+```
+python3 training.py
+```
+
+All simulation parameters can be found at the top of `training.py` and tweaked from there. I provide the parameters that the plots in this README were created with.
+
+Plots can be created by running `visualize.py` as main. In doing so you might have to tweak plotting parameters or even the output directory in the at the end of the file.
+All other modules complete their tests when run as main.
+
+## TODO
 
 Plan:
 - 1. Build MLP model ✓
@@ -15,6 +48,7 @@ Plan:
     - Initial condition loss ✓
 - 5. Add periodic boundary conditions ✓
 - 6. Add visualization and conservation monitoring ✓
-- 7. Reduce memory load
+- 7. Reduce memory load ✓
 - 8. Improve Training parameters
 - 9. Cleanup! Code, Readme (This project was never in a messy state :D)
+- 10. Physics notebook for further explanation
--- visualize.py
+++ visualize.py
@@ -1,74 +1,60 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import torch
+from models import Mlp
+from matplotlib.animation import FuncAnimation
 from utils import *
 from bgk_physics import *
 
-def plot_fxv(model, t_fixed, x_grid, v_grid):
+torch.set_default_dtype(torch.float64)
+
+def plot_fxv(f, t_fixed, x_grid, v_grid, f_path):
     """
     Plot f(x,v) at a fixed time
+    Takes numpy arrays of quantities
     """
-    model.eval()
-    t = torch.full((x_grid.size(0),1), t_fixed, device=x_grid.device)
-    txv = make_txv_stack(t, x_grid, v_grid)
 
-    f = model(txv).detach().cpu().numpy()
-    N_x = x_grid.size(0)
-    N_v = v_grid.size(0)
+    N_x = x_grid.shape[0]
+    N_v = v_grid.shape[0]
 
     f_grid = f.reshape(N_x, N_v)  # x along rows, v along columns
+    f_max = f.max()
 
-    v_grid = v_grid.detach().cpu().numpy()
-    x_grid = x_grid.detach().cpu().numpy()
+    norm = mpl.colors.Normalize(vmin=0.0, vmax=f_max, clip=False)
 
     plt.figure(figsize=(6,5))
     plt.imshow(f_grid, extent=[v_grid[0], v_grid[-1], x_grid[0], x_grid[-1]],
-               origin='lower', aspect='auto', cmap='viridis')
+               origin='lower', aspect='auto', cmap='viridis', norm=norm)
+
     plt.colorbar(label='f(x,v)')
     plt.xlabel('v')
     plt.ylabel('x')
     plt.title(f'f(x,v) at t={t_fixed:.2f}')
+    plt.savefig(f_path)
     plt.show()
 
-def plot_moments(model, t_fixed, x_grid, v_grid):
-    model.eval()
-    t = torch.full((x_grid.size(0),1), t_fixed, device=x_grid.device)
-    txv = make_txv_stack(t, x_grid, v_grid)
-
-    f = model(txv).detach()
-    N_x = x_grid.size(0)
-    N_v = v_grid.size(0)
-    f_grid = f.view(N_x, N_v)
-
-    # Compute moments
-    rho = density(f_grid, v_grid)
-    u = compute_u(f_grid, v_grid)
-    T = temperature(f_grid, v_grid)
-
-    v_grid = v_grid.detach().cpu().numpy()
-    x_grid = x_grid.detach().cpu().numpy()
-    rho = rho.detach().cpu().numpy()
-    u = u.detach().cpu().numpy()
-    T = T.detach().cpu().numpy()
-
-    # Plot
+def plot_moments(rho, u, T, t_fixed, x_grid, moments_path):
+    """
+    Plots moments at fixed time
+    Takes numpy arrays as quantities
+    """
     plt.figure(figsize=(12,4))
 
     plt.subplot(1,3,1)
     plt.plot(x_grid, rho)
-    plt.xlabel('x'); plt.ylabel('density ρ'); plt.title('Density')
+    plt.xlabel('x'); plt.ylabel('density ρ'); plt.title(f"Density at t={t_fixed}")
 
     plt.subplot(1,3,2)
     plt.plot(x_grid, u)
-    plt.xlabel('x'); plt.ylabel('velocity u'); plt.title('Mean velocity')
+    plt.xlabel('x'); plt.ylabel('velocity u'); plt.title(f"Mean velocity at t={t_fixed}")
 
     plt.subplot(1,3,3)
     plt.plot(x_grid, T)
-    plt.xlabel('x'); plt.ylabel('temperature T'); plt.title('Temperature')
+    plt.xlabel('x'); plt.ylabel('temperature T'); plt.title(f"Temperature at t={t_fixed}")
 
     plt.tight_layout()
+    plt.savefig(moments_path)
     plt.show()
-
-from matplotlib.animation import FuncAnimation
 
 """
 def animate_density(model, x_grid, v_grid, t_list):
@@ -98,9 +84,9 @@ def animate_density(model, x_grid, v_grid, t_list):
 
     ani = FuncAnimation(fig, update, frames=len(t_list), blit=False)
     plt.show()
+
 """
-def animate_density(model, x_grid, v_grid, t_list):
-    model.eval()
+def animate_density(model, x_grid, v_grid, t_list, ani_path):
     N_x = x_grid.size(0)
     N_v = v_grid.size(0)
 
@@ -133,4 +119,63 @@ def animate_density(model, x_grid, v_grid, t_list):
         return line,
 
     ani = FuncAnimation(fig, update, interval=500, frames=len(t_list), blit=False)
+    ani.save(ani_path)
     plt.show()
+
+
+#Execute as main for visualization
+if __name__ == "__main__":
+    import torch
+    import models
+    from pathlib import Path
+
+    DIR_PATH = Path("./plots")
+    WEIGHTS = 12000
+    DEVICE = str(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    DTYPE = torch.float64
+
+    #Sampling parameters
+    #Grid parameters
+    N_TX = 128      #Number of points in t and x dimension
+    N_V = 56        #Number of points in v
+
+    X_MIN = -1.0    #spatial grid
+    X_MAX =  1.0
+    T_MIN =  0.0    #temporal grid
+    T_MAX =  1.0
+    T0    = 1.0     #temperature for 
+    V_MAX = 6.5 # 5 * (T0 ** 0.5)
+
+    t_plot = 0.1
+    t_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+    DIR_PATH.mkdir(exist_ok=True)
+
+    f_path = DIR_PATH.joinpath(f"f_{WEIGHTS}.jpg")
+    moments_path = DIR_PATH.joinpath(f"moments_{WEIGHTS}.jpg")
+    ani_path = DIR_PATH.joinpath(f"ani_{WEIGHTS}.gif")
+
+    model = Mlp().to(DEVICE)
+    model.load_state_dict(torch.load(f"./model_weights{WEIGHTS}.pth", weights_only=True))
+    model.eval()
+
+    t_ten = torch.linspace(-T_MIN, T_MAX, N_TX, device=DEVICE).unsqueeze(1)
+    x_ten = torch.linspace(X_MIN, X_MAX, N_TX, device=DEVICE).unsqueeze(1)
+    v_ten = torch.linspace(-V_MAX, V_MAX, N_V, device=DEVICE)
+    txv = make_txv_stack(t_ten, x_ten, v_ten)
+
+    f = model(txv).reshape(N_TX, N_V)
+    rho = density(f, v_ten.unsqueeze(0)).detach().cpu().numpy()
+    u = density(f, v_ten.unsqueeze(0)).detach().cpu().numpy()
+    temperature = temperature(f, v_ten.unsqueeze(0)).detach().cpu().numpy()
+
+
+    t = np.linspace(T_MIN, T_MAX, N_TX)
+    x = np.linspace(X_MIN, X_MAX, N_TX)
+    v = np.linspace(-V_MAX, V_MAX, N_V)
+
+    f_grid = model(txv).detach().cpu().numpy()
+
+    plot_fxv(f_grid, t_plot, x, v, f_path)
+    plot_moments(rho, u, temperature, t_plot, x, moments_path)
+    animate_density(model, x_ten, v_ten, t_list, ani_path)
